e6e3de414d54ee1583656a1ad50fbf7dcf6220ad
Ramy Ibrahim <ramymibrahim@hotmail.com>
2018-12-17 21:37:06 +0200

    tasks

--- helper/posts.php
+++ helper/posts.php
@@ -26,3 +26,32 @@ function getPosts($cat_id=null,$limit=null,$page=1,$condition=null){
 
 return $posts;
 }
+
+function getPost($id){
+    $post=[
+        'id'=>1,
+        'title'=>'Title1',
+        'content'=>'Title1 Title1 Title1',
+        'user_id'=>1,
+        'user_name'=>'Ayman',
+        'comments'=>[
+            [
+                'id'=>1,
+                'content'=>'adasdasd',
+                'user_id'=>1,
+                'user_name'=>'Ayman',
+                'picture'=>'ayman.jpg',
+                'created_at'=>'2018-10-10 12:11:12 am'
+            ],
+            [
+                'id'=>2,
+                'content'=>'adasdasd',
+                'user_id'=>1,
+                'user_name'=>'Ayman',
+                'picture'=>'ayman.jpg',
+                'created_at'=>'2018-10-10 12:11:12 am'
+            ]
+        ]
+    ];
+    return $post;
+}
